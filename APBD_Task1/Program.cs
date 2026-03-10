d2406a3689ef3f6a4d1129639357f1fd6c6e5ef9
Hasan Soner Demir <s30031@pjwstk.edu.pl>
2026-03-10 17:07:36 +0100

    CalculateAverage on feature-average

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿namespace APBD_Task1
+{
+
+
+    class Program
+    {
+
+        static int CalculateAverage(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            int average = sum / 3;
+            return average;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = CalculateAverage(10, 20, 30);
+            Console.WriteLine(result);
+        }
+    }
+}
